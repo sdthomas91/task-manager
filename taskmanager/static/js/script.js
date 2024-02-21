4373280c7a2350f444901699d960daa0fb27fc18
sdthomas91 <130663468+sdthomas91@users.noreply.github.com>
2024-02-21 12:05:24 +0000

    added the functionality to add a new task

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,11 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {}); // Pass an empty object if no options are needed
+
+    // modal open
+
+    let modalOpen = document.querySelectorAll('.modal');
+    M.Modal.init(modalOpen, {});
+
+    // Date Picker
+
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {
+            done: "Select"
+        }
+    });
+
+    // select intitalization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });
 
-// Modal Open
+// // Modal Open
 
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, {});
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//     var elems = document.querySelectorAll('.modal');
+//     var instances = M.Modal.init(elems, {});
+// });
+
+// // Date Picker
+// document.addEventListener('DOMContentLoaded', function () {
+//     var elems = document.querySelectorAll('.datepicker');
+//     var instances = M.Datepicker.init(elems, {
+//         format: "dd mmmm, yyyy",
+//         i18n: {
+//             done: "Select"
+//         }
+//     });
+// });
