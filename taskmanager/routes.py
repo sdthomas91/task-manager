238dd8736dd74a6702621c2cd3ec9a80923c27dd
sdthomas91 <130663468+sdthomas91@users.noreply.github.com>
2024-02-21 10:09:30 +0000

    added new category functionality so that people can add and see categories CR from CRUD

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -2,13 +2,17 @@ from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 
+
 @app.route("/")
 def home():
     return render_template("tasks.html")
 
+
 @app.route("/categories")
 def categories():
-    return render_template("categories.html")
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html", categories=categories)
+
 
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
