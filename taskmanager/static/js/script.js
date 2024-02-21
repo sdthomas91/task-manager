865f62a9bf06cab9a50fdda24637bfdef84b7852
sdthomas91 <130663468+sdthomas91@users.noreply.github.com>
2024-02-21 11:27:23 +0000

    added delete functionality and included a modal for deletion confirmation

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,3 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {}); // Pass an empty object if no options are needed
 });
+
+// Modal Open
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, {});
+});
