ef4ce2dc5e5fa123b4910ef2752ec18841b8bf3d
sdthomas91 <130663468+sdthomas91@users.noreply.github.com>
2024-02-22 12:18:21 +0000

    amended init py to try redeploy

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,32 +1,22 @@
 import os
+import re
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
-    import env
+    import env  # noqa
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 if os.environ.get("DEVELOPMENT") == "True":
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")  # local
 else:
     uri = os.environ.get("DATABASE_URL")
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config["SQLALCHEMY_DATABASE_URI"] = uri
-
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
 
 db = SQLAlchemy(app)
 
-
-
-from taskmanager import routes
-
-
-
-@app.cli.command("create-db")
-def create_db():
-    """Creates the db tables."""
-    db.create_all()
-    print("Database tables created.")
+from taskmanager import routes  # noqa
